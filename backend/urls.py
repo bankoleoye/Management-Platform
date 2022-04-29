13a659451a0c2d33e97c8dadefb9d9d05d0d0564
Bankole Oyewole <66924805+bankoleoye@users.noreply.github.com>
2022-04-29 16:38:08 +0100

    bug fix update

--- backend/urls.py
+++ backend/urls.py
@@ -10,11 +10,10 @@ urlpatterns = [
     path('ticket', CreateTickets.as_view(), name='ticket'),
     path('ticket/<int:pk>', UpdateTickets.as_view(), name='ticket'),
     path('sessions', CreateSession.as_view(), name='sessions'),
-    # path('sessions/<int:pk>', UpdateSession.as_view(), name='sessions'),
+    path('sessions/<int:pk>', UpdateSession.as_view(), name='sessions'),
     path('newsletter', SendNewsLetter.as_view(), name='newsletter'),
     path('sign-up', RegisterUsers.as_view(), name='sign-up'),
     path('update-user/<int:pk>', UpdateUserByCommunityManager.as_view(), name='user'),
     path('user/update/<int:pk>', CeoUpdateUser.as_view(), name='user'),
-    path('user/<int:pk>', UpdateUserByCommunityManager.as_view(), name='user'),
-    path('user_view', ViewAllUsers.as_view(), name='user'),
+    path('user-view', ViewAllUsers.as_view(), name='user'),
 ]
